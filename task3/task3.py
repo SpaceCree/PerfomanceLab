fcdb2117b8f3c0dbbc95fbf2a1e373f8ee7e486e
gadiev <gadiev6667@gmail.com>
2023-12-23 18:15:46 +0300

    main

--- task3/task3.py
+++ task3/task3.py
@@ -1,4 +1,7 @@
 import json
+import os
+import sys
+
 def get_values(tests, results):
     for test in tests:
         test_id = test.get("id")
@@ -6,20 +9,33 @@ def get_values(tests, results):
             test["value"] = results[test_id]["value"]
         if "values" in test:
             get_values(test["values"], results)
-def main():
 
-    with open("tests.json", "r") as tests_file:
+def main(tests_file_path, values_file_path):
+    with open(tests_file_path, "r") as tests_file:
         tests_data = json.load(tests_file)["tests"]
 
-    with open("values.json", "r") as values_file:
+    with open(values_file_path, "r") as values_file:
         values_data = json.load(values_file)["values"]
 
     results = {item["id"]: item for item in values_data}
 
     get_values(tests_data, results)
 
-    with open("report.json", "w") as report_file:
+
+    directory = os.path.dirname(tests_file_path)
+
+
+    report_file_path = os.path.join(directory, "report.json")
+
+    with open(report_file_path, "w") as report_file:
         json.dump(tests_data, report_file, indent=2)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 3:
+        print(" python filename.py tests.json values.json")
+        sys.exit(1)
+
+    tests_file_path = sys.argv[1]
+    values_file_path = sys.argv[2]
+
+    main(tests_file_path, values_file_path)
